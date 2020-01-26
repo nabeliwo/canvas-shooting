ac01e45d404fee6943c02f66dfbac7c106b5f682
nabeliwo <hiroki.c.watanabe@gmail.com>
2020-01-26 22:23:48 +0900

    :sparkles: Add sceneManager

--- script/script.js
+++ script/script.js
@@ -3,14 +3,17 @@
 
   const CANVAS_WIDTH = 640
   const CANVAS_HEIGHT = 480
+  const ENEMY_MAX_COUNT = 10
   const SHOT_MAX_COUNT = 10
 
   let util = null
   let canvas = null
   let ctx = null
+  let scene = null
   let image = null
   let startTime = null
   let viper = null
+  let enemyArray = []
   let shotArray = []
   let singleShotArray = []
 
@@ -24,8 +27,12 @@
   })
 
   function initialize() {
+    let i
+
     canvas.width = CANVAS_WIDTH
     canvas.height = CANVAS_HEIGHT
+
+    scene = new SceneManager()
 
     viper = new Viper(ctx, 0, 0, 64, 64, './image/viper.png')
     viper.setComing(
@@ -35,7 +42,11 @@
       CANVAS_HEIGHT - 100,
     )
 
-    for (let i = 0; i < SHOT_MAX_COUNT; ++i) {
+    for (i = 0; i < ENEMY_MAX_COUNT; ++i) {
+      enemyArray[i] = new Enemy(ctx, 0, 0, 48, 48, './image/enemy_small.png')
+    }
+
+    for (i = 0; i < SHOT_MAX_COUNT; ++i) {
       shotArray[i] = new Shot(ctx, 0, 0, 32, 32, './image/viper_shot.png')
       singleShotArray[i * 2] = new Shot(ctx, 0, 0, 32, 32, './image/viper_single_shot.png')
       singleShotArray[i * 2 + 1] = new Shot(ctx, 0, 0, 32, 32, './image/viper_single_shot.png')
@@ -49,6 +60,10 @@
 
     ready = ready && viper.ready
 
+    enemyArray.map(v => {
+      ready = ready && v.ready
+    })
+
     shotArray.map(v => {
       ready = ready && v.ready
     })
@@ -59,6 +74,7 @@
 
     if (ready === true) {
       eventSetting()
+      sceneSetting()
       startTime = Date.now()
       render()
     } else {
@@ -76,17 +92,42 @@
     })
   }
 
+  function sceneSetting() {
+    scene.add('intro', time => {
+      if (time > 2.0) {
+        scene.use('invade')
+      }
+    })
+
+    scene.add('invade', time => {
+      if (scene.frame !== 0) return
+
+      for (let i = 0; i < ENEMY_MAX_COUNT; ++i) {
+        if (enemyArray[i].life <= 0) {
+          let e = enemyArray[i]
+          e.set(CANVAS_WIDTH / 2, -e.height)
+          e.setVector(0.0, 1.0)
+          break
+        }
+      }
+    })
+
+    scene.use('intro')
+  }
+
   function render() {
     ctx.globalAlpha = 1.0
     util.drawRect(0, 0, canvas.width, canvas.height, '#eee')
     let nowTime = (Date.now() - startTime) / 1000
 
+    scene.update()
     viper.update()
-
+    enemyArray.map(v => {
+      v.update()
+    })
     shotArray.map(v => {
       v.update()
     })
-
     singleShotArray.map(v => {
       v.update()
     })
